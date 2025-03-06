ffaf5e3b9b0db5a8c36e071c11b95264787540bb
Stanislau Minchuk <silkfinik@gmail.com>
2025-03-06 08:55:35 +0000

    update main.c & .replit
    add insert_ascii_codes in main.c & add arg in .replit

--- .ccls-cache/@home@runner@workspace/src@main.c
+++ .ccls-cache/@home@runner@workspace/src@main.c
@@ -1,8 +1,24 @@
-/* hello.c */
 #include <stdio.h>
+#include <string.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+#define MAX_STR_LEN 256
+
+void insert_ascii_codes(const char *input) {
+  char output[MAX_STR_LEN * 5] = "";
+  char temp[10];
+
+  printf("Исходная строка: %s\n", input);
+
+  for (size_t i = 0; i < strlen(input); i++) {
+    sprintf(temp, "%c%d", input[i], input[i]);
+    strcat(output, temp);
+  }
+
+  printf("Результат: %s\n", output);
+}
+
+int main(int argc, char *argv[]) {
+  insert_ascii_codes(argv[1]);
+
+  return 0;
 }
